c8ae21170a03461d1cb120f3196604e106baf092
Christian Whitesides <whitesidescr@gmail.com>
2017-06-13 16:41:24 -0600

    Done.

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MoviesList = ({ movies }) => {
-  const renderMovies = movies.map(movie => 
+  const renderMovies = movies.map(movie =>
     <Link style={{ marginRight: '12px' }} key={movie.id} to={`/movies/${movie.id}`}>{movie.title}</Link>
   );
-  
+
   return (
     <div>
       {renderMovies}
+      <Link to="/movies/new">Add movie</Link>
     </div>
   );
 };
